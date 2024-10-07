3ef8f4e2c42f53eacd82dcd9db2f7f020df37b2c
mateopolci <mateopolci@hotmail.com>
2024-10-07 18:07:00 -0300

    working /rules and /game endpoints

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # RockPaperScissors
+
+Rock Paper Scissors API game made with Go and Gin.
+
+## Endpoints (Initially)
+
+- `GET /rules` -> Returns the game rules.
+- `POST /game` -> Recieves the user's choice and returns the result of the game.
+
+## Dev tools
+
+- Go Air (Hot Reload).
+
+    Install: `go install github.com/air-verse/air@latest`.
+
+    Test version: `air -v`.
+
+    Init: `air init`.
+
+    Go live: `air`.
+
+    Stop: `ctrl + c`.
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"strings"
+	"github.com/gin-gonic/gin"
+)
+
+// Choices
+const (
+	Rock     = "rock"
+	Paper    = "paper"
+	Scissors = "scissors"
+	CW = "Client Wins"
+	SW = "Server Wins"
+)
+
+type UserChoice struct {
+	Choice string `json:"choice"`
+}
+
+// Rules route Controller
+func getRules(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, gin.H{
+		"game": "Rock, paper, scissors",
+		"rules": []gin.H{
+			{
+				"choice":      "Stone",
+				"beats":       "Scissors",
+				"losesTo":     "Paper",
+				"description": "Rock smashes scissors.",
+			},
+			{
+				"choice":      "Paper",
+				"beats":       "Stone",
+				"losesTo":     "Scissors",
+				"description": "Paper covers stone.",
+			},
+			{
+				"choice":      "Scissors",
+				"beats":       "Paper",
+				"losesTo":     "Stone",
+				"description": "Scissors cut paper.",
+			},
+		},
+		"note": "If both players pick the same, it's a draw.",
+	})
+}
+
+// Game route controller
+func postGame(ctx *gin.Context) {
+	var clientChoice UserChoice
+
+	//Bind JSON input to clientChoice and evaluate error
+	err := ctx.BindJSON(&clientChoice)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	//Call and evaluate results
+	clientChoiceString, serverChoice, result := winnerEvaluator(clientChoice)
+	if result == "" {
+		return
+	}
+	ctx.IndentedJSON(http.StatusOK, gin.H{
+		"clientChoice": strings.Title(clientChoiceString),
+		"serverChoice": strings.Title(serverChoice),
+		"result": strings.Title(result),
+	})
+}
+
+// Game result
+func winnerEvaluator(clientChoice UserChoice) (string, string, string) {
+	//Convert user's choice to lowercase
+	loweredClientChoice := strings.ToLower(clientChoice.Choice)
+
+	//Returns empty strings if user's choice is not valid
+	if loweredClientChoice != Rock && loweredClientChoice != Paper && loweredClientChoice != Scissors {
+		return "", "", ""
+	}
+
+	//Establishes server's hand
+	serverChoice := serverChoiceGenerator()
+
+	//Determins winner
+	var result string
+
+	if loweredClientChoice == serverChoice {
+		result = "Draw"
+	} else {
+		switch loweredClientChoice {
+		case Rock:
+			if serverChoice == Scissors {
+				result = CW
+			}else{
+				result = SW
+			}
+		case Paper:
+			if serverChoice == Rock {
+				result = CW
+			}else{
+				result = SW
+			}
+		case Scissors:
+			if serverChoice == Paper {
+				result = CW
+			}else{
+				result = SW
+			}
+		}
+	}
+
+	//Returns
+	return loweredClientChoice, serverChoice, result
+}
+
+// Choice generator
+func serverChoiceGenerator() string {
+	//Stores a number from 0 to 2
+	randomChoice := rand.Intn(3)
+	//Returns the server's choice given the random number
+	if randomChoice == 0 {
+		return Rock
+	} else if randomChoice == 1 {
+		return Paper
+	} else {
+		return Scissors
+	}
+}
+
+// Main
+func main() {
+	router := gin.Default()
+	router.GET("/rules", getRules)
+	router.POST("/game", postGame)
+	router.Run("localhost:8080")
+}
